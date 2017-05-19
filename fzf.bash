94f91b591743769430d13ecd6ae519ce07ac9e6d
Hinckley, Troy J <troy.j.hinckley@intel.com>
2017-05-19 15:16:10 -0700

    added fzf util

---
+++ fzf.bash
@@ -0,0 +1,11 @@
+# Setup fzf
+# ---------
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/nfs/site/home/tjhinckl/.dotfiles/fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/nfs/site/home/tjhinckl/.dotfiles/fzf/shell/key-bindings.bash"
+
