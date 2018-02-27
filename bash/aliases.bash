f40a2c052071860389825ddacc9b801db433118a
Hinckley, Troy J <troy.j.hinckley@intel.com>
2018-02-27 10:03:07 -0800

    updated troy-theme, made bash emacs-compatible, and added utilities

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -14,18 +14,25 @@ alias win-m='xrandr -s 1536x864'
 if [[ $EC_SITE == 'fc' ]]; then
     alias srcenv='eval $(csh-source -inline -noalias /p/hdk/rtl/hdk.rc -cfg shdk73)'
 else
-    alias srcenv='source /p/hdk/rtl/cad/x86-64_linux26/intel/Modules/3.2.10.2/init/bash; module load lv_cfg; unset module'
+    alias srcenv='eval $(csh-source -inline -noalias /p/hdk/rtl/hdk.rc -cfg shdk74)'
+    # source /p/hdk/rtl/cad/x86-64_linux26/intel/Modules/3.2.10.2/init/bash; module load lv_cfg; unset module
 fi
 
+alias phases='gzgrep "started\." acerun.log* | grep sla_tb_env | cut -d"@" -f2'
 alias ports='netstat -lptu'
 alias xterm-custom='xterm -fs 11 -u8'
 
 alias notify-home='exit_code=PASSED || exit_code=FAILED ; $HOME/scripts/notify-home.sh $exit_code'
 alias notify-work='exit_code=PASSED || exit_code=FAILED ; $HOME/scripts/notify-work.sh $exit_code'
 alias notify-all='exit_code=PASSED || exit_code=FAILED ; $HOME/scripts/notify-all.sh $exit_code'
-
 alias check='nbqstat -P $NBPOOL --task-name --priority user=$USER'
 
 alias dsync='$HOME/.dotfiles/dotsync/bin/dotsync'
 alias man-root-mode='export MANUAL_MODEL_ROOT=t'
 alias print-path='echo $PATH | tr : '\''\n'\'''
+alias busy="lsof +D"
+alias mem-usage='ps aux --sort -rss'
+alias code-review='/usr/intel/pkgs/ccollab/7.3.7302/ccollabgui &'
+alias mpar='ag --nonumbers focus_stf | sort | uniq'
+
+alias real-failures="ag --search-zip --depth 1 -G postsim.log -l '(?:requiredtext not found:|jtagbfm|stf_bfm_agent)'"
