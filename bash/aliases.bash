0899d4a6116df17b9fb1e21ddee4202c3201a4f4
Hinckley, Troy J <troy.j.hinckley@intel.com>
2023-07-05 13:59:46 -0700

    Update bash aliases

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -39,7 +39,12 @@ alias run_dft_ipgen='$DFT_REPO_ROOT/DFTNetworkGen/run_dft_ipgen'
 alias real-failures="echo 'BFM failure:'; ag --search-zip --depth 1 -G postsim.log -l '(?:jtagbfm|stf_bfm_agent)'; echo 'early termination:'; ag --search-zip --depth 1 -G postsim.log -l '(?:requiredtext not found:)'"
 alias stf-packets="ag --search-zip --nonumbers '(?:^ovm_error.+?stf_bfm_driver|^actual: |^expect: |^  mask: |^ovm_info.+?\[spf_itpp_parser_info\] \(\d+\)|^ovm_info.+?stf_bfm_driver.+?expected packet passed)'"
 alias spftmp='for i in *.espf; do $SPF_ROOT/bin/spf_perl_pp -stfSpecFile $STF_SPFSPEC -tapSpecFile $TAP_SPFSPEC -testSeqFile $i --itppFile $i.itpp --templateFile $DFT_GLOBAL_DIR/spf_sequences.1p0/tap/sub_system/TAP ; done'
-alias task='nbjob_run_feeder --target sc_normal3 --qslot /DCSG/fe/rgr/gnrio/regress --class "SLES11SP4&&62G"'
-alias fgptask='nbjob_run_feeder --target sc_normal3 --qslot /DCSG/fe/rgr/gnrio/regress --class "SLES11SP4&&5C&&62G&&!SANDYBRIDGE&&!CASCADELAKE&&!HASWELL&&!WESTMERE&&!NEHALEM"'
-alias ijob="nbjob run --target sc_normal3 --qslot /DCSG/fe/rgr/gnrio/regress --mode interactive --class 'SLES11SP4&&62G'"
-alias job="nbjob run --target sc_normal3 --qslot /DCSG/fe/rgr/gnrio/regress --class 'SLES11SP4&&62G'"
+alias task='nbjob_run_feeder --target sc_normal3 --qslot /DCSG/fe/rgr/gnrio/regress --class "SLES12SP5&&62G"'
+alias fgptask='nbjob_run_feeder --target sc_normal3 --qslot /DCSG/fe/rgr/gnrio/regress --class "SLES12SP5&&5C&&62G&&!SANDYBRIDGE&&!CASCADELAKE&&!HASWELL&&!WESTMERE&&!NEHALEM"'
+alias ijob="nbjob run --log-file interactive.nbjob.log --target sc_normal3 --qslot /DCSG/fe/rgr/gnrio/regress --mode interactive --class 'SLES12SP5&&62G' --priority 11"
+alias ijob_func="nbjob run --log-file interactive.nbjob.log --target sc_normal3 --qslot /DCSG/fe/rgr/gnrio/regress --mode interactive --class 'SLES12SP5&&90G' --priority 11"
+alias ijob_mpp="nbjob run --log-file interactive.nbjob.log --target sc_normal3 --qslot /DCSG/fe/rgr/gnrio/regress --mode interactive --class 'SLES12SP5&&100G' --priority 11"
+alias ijob_mpp_func="nbjob run --log-file interactive.nbjob.log --target sc_normal3 --qslot /DCSG/fe/rgr/gnrio/regress --mode interactive --class 'SLES12SP5&&150G' --priority 11"
+alias job="nbjob run --target sc_normal3 --qslot /DCSG/fe/rgr/gnrio/regress --class 'SLES12SP5&&62G'"
+
+alias show_fuse_activity='egrep "SLU_FUSE_SIP_ENV|SLU_FUSE_VF|FUSE_SIP_ENV_ECC|SLU_FUSE_SOC_ENV|SLU_FUSE_SOC_ENV_CHK|GNRIO_SLU_ENV_FUSE|FUSE_BYPASS_BD|SOC_READ_FUSES|SOC_FUSE_BASE_SEQ|SOC_FUSE_BYP|SOC_FUSE_BYP_WARM_RESET|FUSE_RESET_WATCHER|FUSE2RB_BYP|FUSE_SENSE|SIG_ACCESS_STEP"'
